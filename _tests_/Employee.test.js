f1ae437fadb784ada96b831fc22e9af8883acf67
BBT <emmagollan33@gmail.com>
2021-04-25 22:16:27 -0400

    added employee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,29 @@
-const checkIfEqual = require('../lib/random.js');
+const Employee = require ('../lib/Employee');
+const { expectToBe, expectStr, expectNum } = require('../utils/expect');
 
-test('checks if 10 is equal to 10', () => {
-  expect(checkIfEqual(10, 10)).toBe(true);
+const employee = new Employee('Jemma', 'Smith', 1);
+
+test('creates a employee object', () => {
+    expectToBe(employee.firstName,'Jemma');
+    expectToBe(employee.lastName, 'Smith');
+    expectNum(employee.id);
+    expectStr(employee.email, '@');
+    expectToBe(employee.role, 'Employee');
 });
+
+test('gets employee\'s name', () => {
+    expectStr(employee.getName(), `${employee.firstName} ${employee.lastName}`);
+});
+
+test('gets employee\'s ID', () => {
+    expectStr(employee.getId(), `${employee.id}`);
+});
+
+test('gets employee\'s email', () => {
+    expectStr(employee.getEmail(), employee.email);
+});
+
+test('gets employee\'s role', () => {
+    expectStr(employee.getRole(), employee.role);
+});
+
